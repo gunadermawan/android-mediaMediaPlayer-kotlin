8a80c97e5051f8783284d87e86175bc9cde3db8d
itselaras <gunadermawan62@gmail.com>
2022-10-12 06:11:17 +0700

    create fun media player to play and pause audio

--- app/src/main/java/com/gunder/android_mediamediaplayer/MainActivity.kt
+++ app/src/main/java/com/gunder/android_mediamediaplayer/MainActivity.kt
@@ -1,11 +1,58 @@
 package com.gunder.android_mediamediaplayer
 
-import androidx.appcompat.app.AppCompatActivity
+import android.media.AudioAttributes
+import android.media.MediaPlayer
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.gunder.android_mediamediaplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var mediaPlayer: MediaPlayer? = null
+    private var isReady: Boolean = false
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        init()
+        binding.apply {
+            btnPlay.setOnClickListener {
+                if (!isReady) {
+                    mediaPlayer?.prepareAsync()
+                } else {
+                    if (mediaPlayer?.isPlaying as Boolean) {
+                        mediaPlayer?.pause()
+                    } else {
+                        mediaPlayer?.start()
+                    }
+                }
+            }
+            btnPause.setOnClickListener {
+                if (mediaPlayer?.isPlaying as Boolean || isReady) {
+                    mediaPlayer?.stop()
+                    isReady = false
+                }
+            }
+        }
+    }
+
+    private fun init() {
+        mediaPlayer = MediaPlayer()
+        val attribute = AudioAttributes.Builder()
+            .setUsage(AudioAttributes.USAGE_MEDIA)
+            .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+            .build()
+        mediaPlayer?.setAudioAttributes(attribute)
+        val afd = applicationContext.resources.openRawResourceFd(R.raw.guitar_background)
+        try {
+            mediaPlayer?.setDataSource(afd.fileDescriptor, afd.startOffset, afd.length)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        mediaPlayer?.setOnPreparedListener {
+            isReady = true
+            mediaPlayer?.start()
+        }
+        mediaPlayer?.setOnErrorListener { _, _, _ -> false }
     }
 }
